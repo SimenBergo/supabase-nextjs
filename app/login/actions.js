8ef6ae5d76c288d3c00f4f7a0bda6c557cb8acf5
Simen Bergo <bergo.simen@gmail.com>
2024-10-27 15:40:30 +0100

    Fix login redirect to /uno

--- app/login/actions.js
+++ app/login/actions.js
@@ -2,43 +2,43 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { signal } from "@preact/signals";
+import { signal } from '@preact/signals'
 import { createClient } from '@/utils/supabase/server'
 
 export async function login(formData) {
-    const supabase = createClient()
+  const supabase = createClient()
 
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
-    const data = {
-        email: formData.get('email'),
-        password: formData.get('password'),
-    }
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email'),
+    password: formData.get('password'),
+  }
 
-    const { error } = await supabase.auth.signInWithPassword(data)
+  const { error } = await supabase.auth.signInWithPassword(data)
 
-    if (error) {
-        redirect('/error')
-    }
+  if (error) {
+    redirect('/error')
+  }
 
-    revalidatePath('/', 'layout')
-    redirect('/account')
+  revalidatePath('/', 'layout')
+  redirect('/uno')
 }
 
 export async function signup(formData) {
-    const supabase = createClient()
+  const supabase = createClient()
 
-    const data = {
-        email: formData.get('email'),
-        password: formData.get('password'),
-    }
+  const data = {
+    email: formData.get('email'),
+    password: formData.get('password'),
+  }
 
-    const { error } = await supabase.auth.signUp(data)
-    console.log('error', error)
-    if (error) {
-        redirect('/error')
-    }
+  const { error } = await supabase.auth.signUp(data)
+  console.log('error', error)
+  if (error) {
+    redirect('/error')
+  }
 
-    revalidatePath('/', 'layout')
-    redirect('/account')
+  revalidatePath('/', 'layout')
+  redirect('/account')
 }
